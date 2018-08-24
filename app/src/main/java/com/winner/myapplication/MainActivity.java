e86879f91b1a8e5ceccd9153085b883f18a0909c
winner <wachirawit.kan@gmail.com>
2018-08-24 15:41:24 +0700

    first commit

---
+++ app/src/main/java/com/winner/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.winner.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView helloWorld = (TextView) findViewById(R.id.hello_world);
+        helloWorld.setText(R.string.str_hello);
+
+
+    }
+}
